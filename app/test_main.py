e674d1b081e3a27151cd8beecb8eec50a9100ada
veronika219 <Dmytrenko.V.S@nmu.one>
2025-12-23 22:11:24 +0200

    Tests fot check_passwod

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        ("Pass@word1", True),          # валідний пароль
+        ("Pass@12", False),              # занадто короткий
+        ("THISISLONGPASSWORD1@", False), # занадто довгий
+        ("nouppercase1@", False),      # без великої літери
+        ("NoDigits!@", False),         # без цифр
+        ("NoSpecial1", False),         # без спецсимволу
+    ]
+)
+def test_check_password(password, expected):
+    assert check_password(password) == expected, f"Password failed: {password}"
